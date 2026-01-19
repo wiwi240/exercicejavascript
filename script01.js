842f0ba88fafbd24b0c2e00fd04c54326b754fee
wiwi240 <william.mahi14@gmail.com>
2026-01-19 17:45:16 +0100

    ajout script03 et maj des autres

---
+++ script01.js
@@ -0,0 +1,6 @@
+let c= "Bonjour", d= "Monde";
+console.log(c + " " + d);
+
+
+let firstname = prompt ("quel est ton prenom");
+console.log("bonjours  " + firstname );
---
+++ script02.js
@@ -0,0 +1,20 @@
+// 1. On récupère et on transforme la saisie
+let nombre = Number(prompt("De quel nombre veux-tu calculer la factorielle ?"));
+
+// 2. On prépare le coffre-fort
+let resultat = 1;
+
+// 3. La boucle qui fait tout le travail
+for (let i = nombre; i > 0; i--) {
+  resultat = resultat * i;
+    // ICI : Écrivez la ligne qui multiplie "resultat" par "i"
+  
+    // et qui remet le tout dans "resultat"
+}
+
+// 4. On affiche le message final
+// ICI : Écrivez le console.log pour afficher "Le résultat est : " suivi du résultat
+
+
+// 4. On affiche le message final
+console.log("Le résultat est : " + resultat);       
